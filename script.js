4db617240dc387e8bef19fc1f14c575cfd23af01
jyotiwelji <jyotiwelji@gmail.com>
2024-10-01 22:59:43 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+var person = [
+    firstName = "Ali",
+    lastName = "Muhammad",
+    age = 21,
+    city = "Karachi",
+];
+
+alert(firstName + lastName + " is " + age + " years old. He lives in " + city);
+
+
+var Car = [
+    name = "Mercedes",
+    model = "Benz CLA",
+    color = "white",
+    price = 4300000,
+];
+
+
+var mobile = [
+    name = "Iphone",
+    color = "black",
+    price = 300000,
+];
+
+
+var dress = [
+    brand = "J.",
+    color = "Green",
+    fabric = "cotton",
+];
+
+
+var result = [
+    studentName = "Ali",
+    fatherName = "Muhammad",
+    rollNo = 43,
+    percentage = 95,
+    rank = "A+",
+];
+
+
+var financialReport = [
+    assets = 45000,
+    expenses = 32000,
+    profit = 13000,
+];
+
+
+var resume = [
+    personalProfile = "",
+    education = "",
+    skills = "",
+    experience = "",
+];
+
+
+var cosmaticPrice = [
+    foundation = 1000,
+    lipstick = 500,
+    loosePowder = 1000,
+    eyeshade = 2000,
+];
+
+
+var laptop = [
+    company = "Dell",
+    generation = 5,
+    price = 85000,
+];
+
+
+var annualCharges = [
+    studentName = "Ali",
+    monthlyFees = 5000,
+    stationary = 1000,
+    activities = 1000,
+];
